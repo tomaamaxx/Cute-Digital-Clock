457f956d999956ae158e32a1b30d9c4c9e144601
tomaamaxx <161865759+tomaamaxx@users.noreply.github.com>
2024-04-10 13:18:24 -0500

    Create DigitalClock.jsx

---
+++ DigitalClock.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+
+function DigitalClock(){
+
+    return(
+        <div ClassName="clock-container">
+            <div ClassName="clock">
+                <span>00:00:00</span>
+            </div>
+        </div>
+    );
+}
+export default DigitalClock 
